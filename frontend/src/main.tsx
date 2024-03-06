57a81e77b505d90247bf401822dc16f00c5a8101
tikaya <tatiana.chefox@gmail.com>
2024-03-06 13:31:42 +0300

    change main.tsx

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { Providers } from './utils/Providers.tsx'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './utils/Router.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
+    </Providers>
   </React.StrictMode>,
 )
