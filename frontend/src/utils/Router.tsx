9764d014495cb03aed7f6ad107e56cf27ef6aefc
tikaya <tatiana.chefox@gmail.com>
2024-03-13 19:37:16 +0300

    changing the location of the registration component

--- frontend/src/utils/Router.tsx
+++ frontend/src/utils/Router.tsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
-import { Registration } from "../components/Registration";
+import { Registration } from "../components/Registration/Registration";
 
 export const router = createBrowserRouter([
     {
