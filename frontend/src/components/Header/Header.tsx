2c08039f0d72054f34e80d4c35b9a3432bb8800d
tikaya <tatiana.chefox@gmail.com>
2024-03-13 20:25:29 +0300

    adding a main container

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,24 +1,40 @@
 import { Link } from "react-router-dom"
 import logo from "./../../assets/images/Frame.svg"
+import location from "./../../assets/images/location.svg"
+import account from "./../../assets/images/account.svg"
 import styles from "./Header.module.css"
 
 export const Header = () => {
 
     return (
-        <div className={styles.container}>
-            <img src={logo} alt="logo" className={styles.logo} />
-            <div>
-                <ul className={styles.menu}>
-                    <li>
-                        <Link to="" className={styles.link}>Информация о донорстве</Link>
-                    </li>
-                    <li>
-                        <Link to="" className={styles.link}>Врачи</Link>
-                    </li>
-                </ul>
+        <div className={styles.header}>
+            <div className={styles.container}>
+                <img src={logo} alt="logo" className={styles.logo} />
+                <div>
+                    <ul className={styles.menu}>
+                        <li>
+                            <Link to="" className={styles.link}>Информация о донорстве</Link>
+                        </li>
+                        <li>
+                            <Link to="" className={styles.link}>Врачи</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
-           
+            <div className={styles.list}>
+                    <Link to="">
+                        Санкт-Петербург
+                        <img alt="" src={location} />
+                    </Link>
+                    <Link to="">
+                        <img alt="" src={account} />
+                    </Link>
+                    <select role="select" defaultValue={"ru"}>
+                        <option value="ru">ru</option>
+                    </select>
+                </div>
         </div>
+
     )
 }
 
