51c8b38ed69ac21ac0425e57e277a286874dabe4
tikaya <tatiana.chefox@gmail.com>
2024-03-13 20:10:58 +0300

    adding menu

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,17 +1,27 @@
+import { Link } from "react-router-dom"
 import logo from "./../../assets/images/Frame.svg"
 import styles from "./Header.module.css"
 
 export const Header = () => {
+
     return (
-        <div>
+        <div className={styles.container}>
             <img src={logo} alt="logo" className={styles.logo} />
+            <div>
+                <ul className={styles.menu}>
+                    <li>
+                        <Link to="" className={styles.link}>Информация о донорстве</Link>
+                    </li>
+                    <li>
+                        <Link to="" className={styles.link}>Врачи</Link>
+                    </li>
+                </ul>
+            </div>
            
         </div>
     )
 }
 
 /*
- <a href="#" class="menu">Информация о донорстве</a>
-            <a href="#" class="menu">Врачи</a>
             <a href="#"class="menu">Санкт-Петербург<img alt="" src="location.svg"> </a>
             <a href="#"class="menu"><img alt="" src="account.svg"></a> */
