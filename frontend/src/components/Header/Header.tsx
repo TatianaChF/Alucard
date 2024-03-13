fc8cd58b90569286a73227e7f475c841652f6af8
tikaya <tatiana.chefox@gmail.com>
2024-03-13 19:46:58 +0300

    adding logo

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -1,7 +1,17 @@
+import logo from "./../../assets/images/Frame.svg"
+import styles from "./Header.module.css"
+
 export const Header = () => {
     return (
         <div>
-            
+            <img src={logo} alt="logo" className={styles.logo} />
+           
         </div>
     )
 }
+
+/*
+ <a href="#" class="menu">Информация о донорстве</a>
+            <a href="#" class="menu">Врачи</a>
+            <a href="#"class="menu">Санкт-Петербург<img alt="" src="location.svg"> </a>
+            <a href="#"class="menu"><img alt="" src="account.svg"></a> */
