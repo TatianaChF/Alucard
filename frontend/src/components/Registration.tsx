25e53dd249ce01a2c9bcbe87d7e6b9e5f0bd0aa3
tikaya <tatiana.chefox@gmail.com>
2024-03-13 17:12:02 +0300

    aligning label and input

--- frontend/src/components/Registration.tsx
+++ frontend/src/components/Registration.tsx
@@ -6,31 +6,31 @@ export const Registration = () => {
         <div className={styles.container}>
             <Link to="">Назад</Link>
             <form className={styles.form_container}>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="surname">Фамилия</label>
                     <input type="text" name="surname" id="surname" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="name">Имя</label>
                     <input type="text" name="name" id="name" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="patronymic">Отчество</label>
                     <input type="text" name="patronymic" id="patronymic" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="tel">Контактный телефон</label>
                     <input type="tel" name="tel" id="tel" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="email">E-mail</label>
                     <input type="email" name="email" id="email" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="password">Пароль</label>
                     <input type="password" name="password" id="password" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="pas_repeat">Повторите пароль</label>
                     <input type="password" name="pas_repeat" id="pas_repeat" />
                 </div>
