d1fbf7788adcf373da739c90c7d3ac950fbbc126
tikaya <tatiana.chefox@gmail.com>
2024-03-14 19:17:26 +0300

    adding inputs

--- frontend/src/components/Authorization/Authorization.tsx
+++ frontend/src/components/Authorization/Authorization.tsx
@@ -1,5 +1,16 @@
 export const Authorization = () => {
     return (
-        <div></div>
+        <div>
+            <form>
+                <div>
+                    <label htmlFor="email">E-mail</label>
+                    <input type="email" name="email" id="email" />
+                </div>
+                <div>
+                    <label htmlFor="password">Пароль</label>
+                    <input type="password" name="password" id="password" />
+                </div>
+            </form>
+        </div>
     )
 }
