9eb8446b7798dabbda4e863e6ae929f48ae983f6
tikaya <tatiana.chefox@gmail.com>
2024-03-14 19:26:04 +0300

    adding style for inputs

--- frontend/src/components/Authorization/Authorization.tsx
+++ frontend/src/components/Authorization/Authorization.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
+import styles from "./Authorization.module.css"
 
 export const Authorization = () => {
     return (
         <div>
-            <form>
-                <div>
+            <form  className={styles.form_container}>
+                <div className={styles.form_input}>
                     <label htmlFor="email">E-mail</label>
                     <input type="email" name="email" id="email" />
                 </div>
-                <div>
+                <div className={styles.form_input}>
                     <label htmlFor="password">Пароль</label>
                     <input type="password" name="password" id="password" />
                 </div>
