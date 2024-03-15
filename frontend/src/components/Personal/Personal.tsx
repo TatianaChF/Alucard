cbbe7adab2c3e10ec5ce21bbc184d7c4e2902cc3
tikaya <tatiana.chefox@gmail.com>
2024-03-15 12:48:11 +0300

    adding styling for user information

--- frontend/src/components/Personal/Personal.tsx
+++ frontend/src/components/Personal/Personal.tsx
@@ -3,31 +3,31 @@ import avatar from "./../../assets/images/foto.png"
 
 export const Personal = () => {
     return (
-        <div>
+        <div className={styles.container}>
             <div className={styles.container_about}>
                 <img src={avatar} alt="avatar" />
-                <div>
-                    <div>
+                <div className={styles.container_info}>
+                    <div className={styles.info}>
                         <p>ФИО</p>
                         <p>Красова Мария Сергеевна</p>
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <p>Пол</p>
                         <p>женский</p>
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <p>Дата рождения</p>
                         <p>27.08.1999</p>
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <p>Город</p>
                         <p>Санкт-Петербург</p>
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <p>Статус</p>
                         <p>Донор</p>
                     </div>
-                    <div>
+                    <div className={styles.info}>
                         <p>Количество сдачи/<br />получения крови</p>
                         <p>Количество сдачи/<br />получения крови</p>
                     </div>
