a0bc2cbd2bb9fddd72cedd14c8be30254f958f18
tikaya <tatiana.chefox@gmail.com>
2024-03-15 11:04:54 +0300

    adding a main image

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,5 +1,12 @@
+import background from "./../../assets/images/fone.png"
+import styles from "./Home.module.css"
+
 export const Home = () => {
     return (
-        <div></div>
+        <div>
+            <div>
+                <img alt="background" src={background} className={styles.img_main} />
+            </div>
+        </div>
     )
 }
