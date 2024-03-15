8ae331e12e2734fb6d982aa07247802cdf709fe2
tikaya <tatiana.chefox@gmail.com>
2024-03-15 11:57:55 +0300

    adding cards with information

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -1,5 +1,14 @@
 import background from "./../../assets/images/fone.png"
 import styles from "./Home.module.css"
+import inf1 from "./../../assets/images/inf1.png"
+import inf2 from "./../../assets/images/inf2.png"
+import inf3 from "./../../assets/images/inf3.png"
+import inf4 from "./../../assets/images/inf4.png"
+import inf5 from "./../../assets/images/inf5.png"
+import inf6 from "./../../assets/images/inf6.png"
+import inf7 from "./../../assets/images/inf7.png"
+import inf8 from "./../../assets/images/inf8.png"
+import inf9 from "./../../assets/images/inf9.png"
 
 export const Home = () => {
     return (
@@ -11,6 +20,31 @@ export const Home = () => {
                     <p> Информация о донорстве, о врачах, запись на дотацию, карта мед организаций и другая
                         необходимая информация в одном месте</p>
                     <button className={styles.button}>Хочу быть донором</button>
+                </div>
+            </div>
+            <div className={styles.container}>
+                <div className={styles.info}>
+                    <h1>Информация о донорстве</h1>
+                    <div className={styles.card_info}>
+                        <div><img src={inf1} width="400" /></div>
+                        <div><img src={inf2} width="400" /></div>
+                        <div><img src={inf3} width="400" /></div>
+                        <div><img src={inf4} width="400" /></div>
+                        <div><img src={inf5} width="400" /></div>
+                        <div><img src={inf6} width="400" /></div>
+                        <div><img src={inf7} width="400" /></div>
+                        <div><img src={inf8} width="400" /></div>
+                        <div><img src={inf9} width="400" /></div>
+                    </div>
+                </div>
+                <div>
+                    <h1>Врачи</h1>
+                    <div>
+                        <div><img src="катрочка%20врача%201.png" width="600" /></div>
+                        <div><img src="катрочка%20врача%202.png" width="600" /></div>
+                        <div><img src="катрочка%20врача%203.png" width="600" /></div>
+                        <div><img src="катрочка%20врача%204.png" width="600" /></div>
+                    </div>
                 </div>
             </div>
         </div>
