efda699b8aa55884aaa3a0b7db0b9a0d098e2d1a
tikaya <tatiana.chefox@gmail.com>
2024-03-15 11:38:36 +0300

    adding a main title and description

--- frontend/src/components/Home/Home.tsx
+++ frontend/src/components/Home/Home.tsx
@@ -6,6 +6,12 @@ export const Home = () => {
         <div>
             <div>
                 <img alt="background" src={background} className={styles.img_main} />
+                <div className={styles.main_container}>
+                    <p><span className={styles.name}>Алукард</span>  - <b>сервис для доноров, где есть все важное</b></p>
+                    <p> Информация о донорстве, о врачах, запись на дотацию, карта мед организаций и другая
+                        необходимая информация в одном месте</p>
+                    <button className={styles.button}>Хочу быть донором</button>
+                </div>
             </div>
         </div>
     )
