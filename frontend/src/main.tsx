a492d74af03a8153ddb54975cf789c16bcb83191
tikaya <tatiana.chefox@gmail.com>
2024-03-16 13:36:11 +0300

    change import

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,7 +4,7 @@ import './index.css'
 import { Providers } from './utils/Providers.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './utils/Router.tsx'
-import { Provider } from 'react-redux/dist/react-redux'
+import { Provider } from 'react-redux'
 import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
