1ecb621e31e682582c7a1bca1b670e3215748fe5
tikaya <tatiana.chefox@gmail.com>
2024-03-16 13:31:00 +0300

    wrapped the main component in a side

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,11 +4,15 @@ import './index.css'
 import { Providers } from './utils/Providers.tsx'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './utils/Router.tsx'
+import { Provider } from 'react-redux/dist/react-redux'
+import { store } from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Providers>
-      <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
+      <Provider store={store}>
+        <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
+      </Provider>
     </Providers>
   </React.StrictMode>,
 )
