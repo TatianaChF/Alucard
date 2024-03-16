58b806558b4fe3a1de39cb16f464efba9040e844
tikaya <tatiana.chefox@gmail.com>
2024-03-16 14:01:56 +0300

    adding type from registartion data

---
+++ frontend/src/redux/registration/types.ts
@@ -0,0 +1,9 @@
+export type registrationData = {
+    userId: string,
+    userSurname: string,
+    userName: string,
+    userPatronymicSurname: string,
+    userTel: string,
+    userPassword: string,
+    isAuth: boolean
+}
