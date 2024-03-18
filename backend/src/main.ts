ac268a4314768acb3bb9eea16aa25db17ebebca1
tikaya <tatiana.chefox@gmail.com>
2024-03-18 09:52:14 +0300

    adding code to main file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,23 +1,31 @@
-import express, { Express } from "express";
-import dotenv from "dotenv";
-import { router } from "./controller.js";
+import { Request, Response } from 'express';
 
-dotenv.config();
+interface Error {
+    status?: number;
+    message: string
+}
 
-const app: Express = express();
-const port = process.env.PORT || 3000;
+require('dotenv').config();
+const cors = require('cors');
+const express = require('express');
 
-app.use("/", router);
+const port = process.env.PORT;
 
-const server = app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
+const testRoutes = require("#routes/test");
+
+const errorHandler = (error: Error, req: Request, res: Response, next: any) => {
+    const status = error.status || 422;
+    res.status(status).send(error.message);
+}
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/api', testRoutes);
+app.use(errorHandler);
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`);
 });
-
-const cleanup = () => {
-    server.close(() => {
-        console.log("shutting down");
-    });
-};
-
-process.on("SIGINT", cleanup);
-process.on("SIGTERM", cleanup);
