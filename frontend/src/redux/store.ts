981bf837d6b0cfcd2b141230e5dc721209971f3c
tikaya <tatiana.chefox@gmail.com>
2024-03-19 18:29:55 +0300

    rename reducer

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,13 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { useDispatch } from "react-redux";
-import selectionSlice from "./selection/selectionSlice";
+import selectionReducer from "./selection/selectionSlice"
 
 export const store = configureStore({
     reducer: {
-        selection: selectionSlice
+        selection: selectionReducer
     }
 })
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
 export const useAppDispatch: () => AppDispatch = useDispatch
+
