b905d52833c4f0d26ceacae80c223acb0d23b20b
tikaya <tatiana.chefox@gmail.com>
2024-03-20 11:45:39 +0300

    adding mandatory fields

--- frontend/src/components/HospitalRegistration/HospitalRegistration.tsx
+++ frontend/src/components/HospitalRegistration/HospitalRegistration.tsx
@@ -30,6 +30,9 @@ export const HospitalRegistration = () => {
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+
+        if (formData.lastname.length < 0) console.log("Введите фамилию!")
+
         dispatch(addInfo(formData));
         setIsRegister(true);
     };
@@ -46,46 +49,46 @@ export const HospitalRegistration = () => {
                             <form onSubmit={handleSubmit} className={styles.form_container}>
                                 <div className={styles.form_input}>
                                     <label htmlFor="lastname">Фамилия:</label>
-                                    <input type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} />
+                                    <input required type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} />
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="firstname">Имя:</label>
-                                    <input type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChange} />
+                                    <input required type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChange} />
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="otch">Отчество:</label>
-                                    <input type="text" id="otch" name="otch" value={formData.otch} onChange={handleChange} />
+                                    <input required type="text" id="otch" name="otch" value={formData.otch} onChange={handleChange} />
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="phone">Контактный телефон:</label>
-                                    <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
+                                    <input required type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="email">Email:</label>
-                                    <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                                    <input required type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="service">Запись на:</label>
-                                    <select id="service" name="service" value={formData.service} onChange={handleChange}>
+                                    <select required id="service" name="service" value={formData.service} onChange={handleChange}>
                                         <option>Выберите из списка</option>
                                         {services.map((service) => <option key={service}>{service}</option>)}
                                     </select>
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="hospital">Выберите медицинское учреждение:</label>
-                                    <select id="hospital" name="hospital" value={formData.hospital} onChange={handleChange}>
+                                    <select required id="hospital" name="hospital" value={formData.hospital} onChange={handleChange}>
                                         <option>Выберите из списка</option>
                                         {hospitals.map((hospital) => <option key={hospital}>{hospital}</option>)}
                                     </select>
                                 </div>
                                 <div className={styles.form_input}>
                                     <label htmlFor="time">Выберите медицинское учреждение:</label>
-                                    <select id="time" name="time" value={formData.time} onChange={handleChange}>
+                                    <select required id="time" name="time" value={formData.time} onChange={handleChange}>
                                         <option>Выберите из списка</option>
                                         {times.map((time) => <option key={time}>{time}</option>)}
                                     </select>
                                 </div>
-                                <button type="submit">Submit</button>
+                                <button type="submit">Записаться</button>
                             </form>
                         </div>
                     </div>
