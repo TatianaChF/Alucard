6dce2cdd5a3421e60018ec4cd92c07d915f9d13e
tikaya <tatiana.chefox@gmail.com>
2024-03-20 10:30:35 +0300

    adding state to record

--- frontend/src/components/HospitalRegistration/HospitalRegistration.tsx
+++ frontend/src/components/HospitalRegistration/HospitalRegistration.tsx
@@ -4,10 +4,42 @@ import { AuthGuard } from "../AuthGuard/AuthGuard"
 import { useDispatch } from "react-redux"
 import { useSelector } from "react-redux"
 import { selectionSelector } from "../../redux/selection/selectors"
+import { ChangeEvent, FormEvent, useState } from "react"
+import { addInfo } from "../../redux/registrationClinic/registrationClinicSlice"
 
 export const HospitalRegistration = () => {
-    const {services, hospitals, times} = useSelector(selectionSelector);
+    const [formData, setFormData] = useState({
+        lastname: "",
+        firstname: "",
+        otch: "",
+        phone: "",
+        email: "",
+        service: "",
+        hospital: "",
+        time: ""
+    });
+    const { services, hospitals, times } = useSelector(selectionSelector);
     const dispatch = useDispatch();
+
+    const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+        const { name, value } = event.target;
+        setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+    };
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        dispatch(addInfo(formData));
+        console.log(`
+        Фамилия: ${formData.lastname}
+        Имя: ${formData.firstname}, 
+        Отчество: ${formData.otch}, 
+        Телефон: ${formData.phone},
+        Email: ${formData.email}, 
+        Направление на: ${formData.service},
+        Медицинское учреждение: ${formData.hospital},
+        Время: ${formData.time}
+        `);
+    };
 
     return (
         <AuthGuard>
@@ -17,45 +49,49 @@ export const HospitalRegistration = () => {
                     <div>
                         <h1>Запись на прием</h1>
                         <h3>Имеются противопоказания к донорству</h3>
-                        <form className={styles.form_container}>
+                        <form onSubmit={handleSubmit} className={styles.form_container}>
                             <div className={styles.form_input}>
-                                <label htmlFor="surname">Фамилия</label>
-                                <input type="text" name="surname" id="surname" />
+                                <label htmlFor="lastname">Фамилия:</label>
+                                <input type="text" id="lastname" name="lastname" value={formData.lastname} onChange={handleChange} />
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="name">Имя</label>
-                                <input type="text" name="name" id="name" />
+                                <label htmlFor="firstname">Имя:</label>
+                                <input type="text" id="firstname" name="firstname" value={formData.firstname} onChange={handleChange} />
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="patronymic">Отчество</label>
-                                <input type="text" name="patronymic" id="patronymic" />
+                                <label htmlFor="otch">Отчество:</label>
+                                <input type="text" id="otch" name="otch" value={formData.otch} onChange={handleChange} />
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="tel">Контактный телефон</label>
-                                <input type="tel" name="tel" id="tel" />
+                                <label htmlFor="phone">Контактный телефон:</label>
+                                <input type="tel" id="phone" name="phone" value={formData.phone} onChange={handleChange} />
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="priem">Запись на</label>
-                                <select name="priem">
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" id="email" name="email" value={formData.email} onChange={handleChange} />
+                            </div>
+                            <div className={styles.form_input}>
+                                <label htmlFor="service">Запись на:</label>
+                                <select id="service" name="service" value={formData.service} onChange={handleChange}>
                                     <option>Выберите из списка</option>
-                                    { services.map((service) => <option>{service}</option>) }
+                                    {services.map((service) => <option key={service}>{service}</option>)}
                                 </select>
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="medorg">Выберите медицинское учреждение</label>
-                                <select name="medorg">
+                                <label htmlFor="hospital">Выберите медицинское учреждение:</label>
+                                <select id="hospital" name="hospital" value={formData.hospital} onChange={handleChange}>
                                     <option>Выберите из списка</option>
-                                    { hospitals.map((hospital) => <option>{hospital}</option>) }
+                                    {hospitals.map((hospital) => <option key={hospital}>{hospital}</option>)}
                                 </select>
                             </div>
                             <div className={styles.form_input}>
-                                <label htmlFor="time">Выберите время</label>
-                                <select name="time">
+                                <label htmlFor="time">Выберите медицинское учреждение:</label>
+                                <select id="time" name="time" value={formData.time} onChange={handleChange}>
                                     <option>Выберите из списка</option>
-                                    { times.map((time) => <option>{time}</option>) }
+                                    {times.map((time) => <option key={time}>{time}</option>)}
                                 </select>
                             </div>
-                            <button>Записаться</button>
+                            <button type="submit">Submit</button>
                         </form>
                     </div>
                 </div>
